1000808fed4b0cb15689781697e3a50a06f4408f
carlodp <santos.carlodp@gmail.com>
2022-08-15 17:13:56 +0800

    transition styles landing section

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import ScrollReveal from 'scrollreveal';
+import { navDelay, loaderDelay } from '../utils/index';
 
 export default function LandingSection(props) {
   const {
@@ -8,15 +11,38 @@ export default function LandingSection(props) {
     shortdesc,
   } = props;
 
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsMounted(true), navDelay);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const one = <span className="before-title">{beforetitle}</span>;
+  const two = (
+    <>
+      <h1 className="title">
+        <strong>{title}</strong>
+        <span>{subtitle}</span>
+      </h1>
+    </>
+  );
+
+  const three = <p className="short-description">{shortdesc}</p>;
+  
+  const items = [one, two, three];
+
   return (
     <React.Fragment>
       <section className="landing">
-        <span className="before-title">{beforetitle}</span>
-        <h1 className="title">
-            <strong>{title}</strong>
-            <span>{subtitle}</span>
-        </h1>
-        <p className="short-description">{shortdesc}</p>
+        <TransitionGroup component={null}>
+          {isMounted &&
+            items.map((item, i) => (
+              <CSSTransition key={i} classNames="fadeup" timeout={loaderDelay}>
+                <div style={{ transitionDelay: `${i + 1}00ms` }}>{item}</div>
+              </CSSTransition>
+            ))}
+        </TransitionGroup>
       </section>
     </React.Fragment>
   );
